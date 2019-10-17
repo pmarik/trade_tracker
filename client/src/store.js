c94e6939a87cec0f6fece2eff6c09cc2bf03db80
pmarik <pmarik@uwalumni.com>
2019-10-16 20:04:32 -0500

    added composeEnhancers to store to fix redux devtools bug for cross-browser support

--- client/src/store.js
+++ client/src/store.js
@@ -5,9 +5,10 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
